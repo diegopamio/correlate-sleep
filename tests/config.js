3668ed157884146ac4a3a1593b5b6c146c92f45a
Diego Pamio (Intel) <diego.pamio@gmail.com>
2014-02-12 18:25:04 -0300

    Added protractor dummy tests.

---
+++ tests/config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by dpamio on 12/02/14.
+ */
+
+exports.config = {
+    sauceUser: "diego_pamio",
+    sauceKey: '610a8049-7d40-4e17-a381-944bf4b19898',
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome',
+        'proxy': {
+            'proxyType': 'manual',
+            'httpProxy': 'http://proxy-us.intel.com:911',
+            'httpsProxy': 'http://proxy-us.intel.com:911'
+        }
+    },
+
+    // Spec patterns are relative to the location of the spec file. They may
+    // include glob patterns.
+    specs: ['*-spec.js'],
+    verbose: true,
+
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true // Use colors in the command line report.
+    }
+};
