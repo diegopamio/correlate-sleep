159cf6f709e4c79be44ff103ef62e92349f927dd
Diego Pamio (Intel) <diego.pamio@gmail.com>
2014-02-13 17:35:50 -0300

    Last attempt before moving to travis...

--- tests/config.js
+++ tests/config.js
@@ -9,7 +9,8 @@ exports.config = {
     capabilities: {
         'browserName': 'chrome',
         'name': process.env.CI_MESSAGE || 'Ad hoc message',
-        'build': process.env.CI_BUILD_NUMBER + ' (' + (process.env.CI_COMMIT_ID || "No comments.").substring(0, 7) + ')'
+        'build': process.env.CI_BUILD_NUMBER + ' (' + (process.env.CI_COMMIT_ID || "No comments.").substring(0, 7) + ')',
+        'tunnel-identifier': process.env.CI_BUILD_NUMBER
     },
     specs: ['*-spec.js'],
     verbose: true,
