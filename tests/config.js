b828ca23aa9cfa06e49b83767ec64ff8295a9351
Diego Pamio (Intel) <diego.pamio@gmail.com>
2014-02-13 17:48:51 -0300

    Moving to travis-ci

--- tests/config.js
+++ tests/config.js
@@ -10,7 +10,7 @@ exports.config = {
         'browserName': 'chrome',
         'name': process.env.CI_MESSAGE || 'Ad hoc message',
         'build': process.env.CI_BUILD_NUMBER + ' (' + (process.env.CI_COMMIT_ID || "No comments.").substring(0, 7) + ')',
-        'tunnel-identifier': process.env.CI_BUILD_NUMBER
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     },
     specs: ['*-spec.js'],
     verbose: true,
